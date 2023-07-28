762888bd02671728e728adabb2ec5773cd421efb
Hariharan0022 <113664913+Hariharan0022@users.noreply.github.com>
2023-07-28 11:33:55 +0530

    Add files via upload

---
+++ Crudmodel.java
@@ -0,0 +1,39 @@
+package com.iamneo.security.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="employee")
+public class Crudmodel {
+      @Id
+      @Column(name="empid")
+      private int empid;
+      @Column(name="empname")
+      private String empname;
+      @Column(name="dob")
+      private String dob;
+      @Column(name="email")
+      private String email;
+      @Column(name="password")
+      private String password;
+      @Column(name="gender")
+      private String gender;
+      @Column(name="mobile")
+      private Long mobile;
+      @Column(name="address")
+      private String address;
+   
+      
+}
